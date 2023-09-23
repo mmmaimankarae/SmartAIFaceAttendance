ce03001df5c9964a27767fc6e1f47f5d9dd44d6e
mmmaimankarae <mineteacha@hotmail.com>
2023-09-23 13:17:38 +0700

    3'rd commiting

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -3,34 +3,47 @@ import cv2
 import face_recognition
 import pickle
 
-    # Import student image into list
+    # IMPORT STUDENT IMAGE INTO LIST #
 facePath = 'Images'
-facePathList = os.listdir(facePath) # list of name.file
+facePathList = os.listdir(facePath) # list of facename.file
 faceList = []
 studentIDList = []
+    
 for path in facePathList:
-    faceList.append(cv2.imread(os.path.join(facePath, path)))
-    studentIDList.append(os.path.splitext(path)[0]) # split name and .file, fill name(ID) into list
-
-# covert face to number (keep it in array, 128 measure point identify the face)
+    if path == ".DS_Store":
+        continue
+    # keep path of mode
+    image = cv2.imread(os.path.join(facePath, path))
+    if image is not None:
+        faceList.append(image)
+        # split name and .file, fill name(ID) into list
+        studentIDList.append(os.path.splitext(path)[0])
+    else:
+        print(f"Unable to read image: {path}")
+    
+    # TRANSFORM OBJECT TO BIT #
 def findEncoding(imagesList):
     encodeLit = []
     for image in imagesList:
         # change color BGR(openCV) to RGB(faceRecognition)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # encoding tha image of face (128 measure)
+        # encoding the image of face (128 measure point identify)
         encode = face_recognition.face_encodings(image)[0]
         encodeLit.append(encode)
     return  encodeLit
 
 print("Encoding Started...")
+# call method for encoding
 encodeFaceList = findEncoding(faceList)
-encodeListAndID = [encodeFaceList, studentIDList] # storage data by list (keep array encoding with ID)
+# storage data by list (keep array encoding with ID)
+encodeListAndID = [encodeFaceList, studentIDList]
 print("Encoding Complete")
-# Create file .p and write(w) with binary(b) mode (suitable for picture)
+
+    # CREATE FILE FOR ENCODDING OBJECT PYTHON #
+# write(w) with binary(b) mode (suitable for picture)
 file = open("EncodeFace.p", 'wb')
-# Write (pickle) data list of array encoding and ID (Data = Object of python that can save and recall to use)
+# write data list of array encoding and ID
 pickle.dump(encodeListAndID, file)
-# Close file (After that can not write)
+# close file (After that can not write)
 file.close()
 print("File Save")
--- main.py
+++ main.py
@@ -1,44 +1,89 @@
+# manage about os (computer directory)
 import os
+
+# transform object to bit (serializing), bit to object (de-serializing)
 import pickle
 
+# manage about image
 import cv2
 
-    # Start the Camera
-cap = cv2.VideoCapture(0) # Open Camera (Order of cam)
+# detect object
+import cvzone
+
+# face detection
+import face_recognition
+import numpy as np
+
+# START THE CAMERA #
+cap = cv2.VideoCapture(0)  # open Camera (order of cam)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-backGround = cv2.imread('Interface/background.png') # import background
+backGround = cv2.imread('Interface/background.JPG')  # import background
 
-    # Import mode of check attendance in the list
+# IMPORT MODE OF CHECK ATTENDANCE IN THE LIST #
 modePath = 'Interface/Modes'
-modePathList = os.listdir(modePath) # list of name.file
+modePathList = os.listdir(modePath)  # list of modename.file
 modeList = []
 
 for path in modePathList:
+    # keep path of mode
     modeList.append(cv2.imread(os.path.join(modePath, path)))
 
-    # Lode the encoding file
+    # LODE THE ENCODING FILE #
 print("Loading Encode File...")
-# Read(r) file .p that data is binary mode
+# read(r) file .p that data is binary mode(b)
 file = open('EncodeFace.p', 'rb')
-# Open (Unpickle) to load data into variable
+# open (unpickle) to load data into variable
 encodeListAndID = pickle.load(file)
-# Close file (After that can not read)
+# close file (after that can not read)
 file.close()
-# Separate data is 128 measure point and ID
+# separate data is 128 measure point and ID
 encodeFaceList, studentIDList = encodeListAndID
 print("Encode File Loaded")
 
-
 while True:
-    success, image = cap.read() # 1. Capture from camera by frame:frame
+    success, image = cap.read()  # capture from camera by frame:frame
 
-    # Define start point and end point on GUI >> start y: end y, start x: end x
-    backGround[162:(162 + 480), 55:(55 + 640)] = image # point of Camera
-    backGround[0:(0 + 800), 780:(780 + 500)] = modeList[0] # point of Modes
+        # RESIZE THE IMAGE TO ITS SMALLER 25% #
+    imageResize = cv2.resize(image, (0, 0), None, 0.25, 0.25)
+    # change color RGB(faceRecognition) to BGR(openCV)
+    imageResize = cv2.cvtColor(imageResize, cv2.COLOR_BGR2RGB)
 
-    cv2.imshow("Face Attendance", backGround) # 2. Create display(GUI)
+    # DETECT FACE ON THE CAMARE #
+    # return list pixel points of face (top, right, bottom, left)
+    faceCurrent = face_recognition.face_locations(imageResize)
 
-    # 3. receive action from User on GUI
+        # ENCODING UNKNOW NEW FACE #
+    encodeNewface = face_recognition.face_encodings(imageResize, faceCurrent)
+
+    # define start point and end point on GUI (start y: end y, start x: end x)
+    backGround[162:(162 + 480), 55:(55 + 640)] = image  # point of Camera
+    backGround[0:(0 + 800), 780:(780 + 500)] = modeList[0]  # point of modes
+
+        # COMPARE ENCODING OF UNKNOW FACE AND KNOW FACE #
+    # make loop of both together
+    for encodeFace, faceLocation in zip(encodeNewface, faceCurrent):
+        # compare if match, that return True
+        matches = face_recognition.compare_faces(encodeFaceList, encodeFace)
+        # calculate distance on both of face and return float of different
+        faceDistance = face_recognition.face_distance(encodeFaceList, encodeFace)
+        print("matches", matches)
+        print("faceDis", faceDistance)
+
+        #matchIndex = np.argmin(faceDistance)
+        # print("match index ", matchIndex)
+
+        #if matches[matchIndex]:
+            # print("known face detected")
+            # print(studentIDList)
+
+            #y1, x2, y2, x1 = faceLocation
+            #y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            #bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            #backGround = cvzone.cornerRect(backGround, bbox, rt=0)
+
+    cv2.imshow("Face Attendance", backGround)  # create GUI (display)
+
+    # receive action from User on GUI
     cv2.waitKey(1)
